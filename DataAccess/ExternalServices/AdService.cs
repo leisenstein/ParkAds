85783d2a6ced2f164d780eb08024c9a5dceec5cf
Georgi Karavasilev <gkaravasilev@gmail.com>
2017-11-15 13:57:50 +0100

    Creating Ad domain and Http request to external service

---
+++ DataAccess/ExternalServices/AdService.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ExternalServices
+{
+    public class AdService
+    {
+        public async Task<IEnumerable<Ad>> Get()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://dm.sof60.dk:81/");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("api/ad").ConfigureAwait(false);
+            var jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonConvert.DeserializeObject<IEnumerable<Ad>>(jsonResponse);
+        }
+    }
+}
