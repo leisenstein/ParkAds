7be6b817410bccdb65eb0a5a147a6d2d13ea01ba
Georgi Karavasilev <gkaravasilev@gmail.com>
2017-11-26 00:52:01 +0100

    Creating reservationDTO + api controller

---
+++ DataTransferObjects/ReservationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTransferObjects
+{
+    public class ReservationDTO
+    {
+        public DateTime DateTime { get; set; }
+        //public UserDTO UserDTO { get; set; }
+
+    }
+}
---
+++ Gateway/Controllers/ReservationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MicroServices.Services;
+using Newtonsoft.Json;
+using DataTransferObjects;
+
+namespace Gateway.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/reservation")]
+    public class ReservationController : Controller
+    {
+        private ReservationEnrichmentService reservationEnrichmentService = new ReservationEnrichmentService();
+        // POST: api/reservation
+        [HttpPost]
+        public ContentResult Post([FromBody] ReservationDTO reservationDTO)
+        {
+            //User user = UserMapping.MapDTOToDomainObject(reservationDTO);
+            reservationEnrichmentService.CreateReservation();
+            return new ContentResult()
+            {
+                Content = "reservation",
+                ContentType = "text/plain",
+                StatusCode = 200
+            };
+            //else
+            //    return new ContentResult()
+            //    {
+            //        StatusCode = 412
+            //    };
+        }
+    }
+}
