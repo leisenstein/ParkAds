eead976028d043ef418639ebd6565f459461786a
Teodor Stoyanov <teooor121@abv.bg>
2018-01-20 00:54:56 +0100

    added logger and memory cache

--- Gateway/Controllers/AdController.cs
+++ Gateway/Controllers/AdController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using MicroServices.Services;
 using DataTransferObjects;
 using Gateway.Mapping;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace Gateway.Controllers
 {
@@ -15,11 +17,29 @@ namespace Gateway.Controllers
     public class AdController : Controller
     {
         private AdMicroService adMicroService = new AdMicroService();
-
+        private IMemoryCache cache;
+        private readonly ILogger<AdMicroService> _logger;
+        public AdController(IMemoryCache cache, ILogger<AdMicroService> logger)
+        {
+            this.cache = cache;
+            _logger = logger;
+        }
         [HttpGet]
         public AdDTO Get()
         {
-            return AdMapping.MapDomainToDTOObject(adMicroService.Get());
+            MemoryCacheEntryOptions cacheExpirationOptions = new MemoryCacheEntryOptions();
+            cacheExpirationOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(2));
+            string key = "ad";
+            AdDTO ad ;
+            if (!cache.TryGetValue<AdDTO>(key, out ad))
+            {
+                do
+                    ad = AdMapping.MapDomainToDTOObject(adMicroService.Get());
+                while (ad.AdEncoding.Length < 100);
+                cache.Set<AdDTO>(key, ad, cacheExpirationOptions);
+            }
+            _logger.LogInformation("Ad request ,"+DateTime.UtcNow);
+            return (AdDTO)cache.Get("ad");
         }
     }
 }
